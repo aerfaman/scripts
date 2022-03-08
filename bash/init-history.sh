1e1384b9b1c5f07b55b0c0b4bed230782e684048
Andre Wu <aerfaman@gmail.com>
2022-03-08 16:39:33 +0800

    1

---
+++ bash/init-history.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# some centos history settings 
+echo "Creating history.sh file to /etc/profile.d/ ......."
+cat >/etc/profile.d/history.sh <<EOF
+USER_IP=`who -u am i 2>/dev/null | awk '{print $NF}' | sed -e 's/[()]//g'`
+if [ -z $USER_IP ]
+then
+USER_IP=`hostname`
+fi
+
+export HISTTIMEFORMAT="[%Y.%m.%d %H:%M:%S][${USER_IP}-${LOGNAME}]"
+export HISTSIZE=10000
+EOF
+
+echo "file created , please relogin this session to check history settings."
